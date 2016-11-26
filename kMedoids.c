1ff36838761883e717d368b0e4484ed540700832
sdi0000111 <sdi0000111@di.uoa.gr>
2016-11-26 11:52:53 +0200

    Addition.

---
+++ kMedoids.c
@@ -0,0 +1,16 @@
+/* SOFTWARE DEVELOPMENT FOR ALGORITHMIC PROBLEMS
+   ASSIGNMENT 2
+   LELEGIANNIS IOANNIS: 1115201200090
+   POULIDIS NIKOLAOS: 1115200000111 */
+   
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h> 
+ 
+int main(int argc,char* argv[]) 
+{
+	int i;
+	char *iPath=NULL,*cPath=NULL,*oPath=NULL;
+	
+	
