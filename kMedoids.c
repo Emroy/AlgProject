ff6588a01e46590a6f4c835ec31729bf0723d210
Lelegiannis Ioannis <sdi1200090@di.uoa.gr>
2016-11-26 12:08:20 +0200

    Worked on kMedoids.c

--- kMedoids.c
+++ kMedoids.c
@@ -7,10 +7,28 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h> 
- 
+
 int main(int argc,char* argv[]) 
 {
+	/*Read command line arguements*/
 	int i;
-	char *iPath=NULL,*cPath=NULL,*oPath=NULL;
+	char* outFilePath;
+	for(i = 1;i < argc;i++){
+		if(!strcmp(argv[i],"-d")){
+			/*call function for evaluating input*/
+		}
+		else if(!strcmp(argv[i],"-c")){
+			/*call function for evaluating configuration*/
+		}
+		else if(!strcmp(argv[i],"-o")){
+			outFilePath = argv[i];
+		}
+		else{
+			fprintf(stderr,"Invalid parameters given. Call should be:\n")
+			fprintf(stderr,"./medoids -d <input file> -c <configuration file> -o <output file>\n");
+			return 0;
+		}
+	}
 	
-	
+	return 0;
+}
