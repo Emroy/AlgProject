be5053610688c433ad8820067a93f0f1952ae1aa
Lelegiannis Ioannis <sdi1200090@di.uoa.gr>
2017-01-15 22:56:11 +0200

    Changes

--- Part3 - recommendations/recommendations.c
+++ Part3 - recommendations/recommendations.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include "Ratings.h"
 #include "Clustering.h"
+#include <time.h>
 
 int main(int argc,char* argv[]){
 	int i = 0;
@@ -27,6 +28,103 @@ int main(int argc,char* argv[]){
 			fprintf(stderr,"\"%s\" is not a valid command line command\n",argv[i]);
 			return -1;
 		}
+	}
+
+	FILE* outputFile = fopen(outputFilePath,"w");
+	if(outputFile == NULL){
+		perror("Failed to open output file");
+		return -1;
+	}
+	fclose(outputFile);
+
+	double hammingLSH_error = 0.0;
+	double euclideanLSH_error = 0.0;
+	double cosineLSH_error = 0.0;
+	for(i=0;i<NUM_OF_FOLDS;i++){
+		Ratings train = getRatingTrainSet();
+		Ratings test = getRatingTestSet();
+
+		lsh_init(train,'h');
+
+		outputFile = fopen(outputFile,"a");
+		if(outputFile == NULL){
+			perror("Failed to open outputFile");
+		}
+
+		fprintf(outputFile,"Hamming LSH fold number %d\n",i);
+
+		unsigned int numOfUsers = ratings_getNumberOfUsers(train);
+		unsigned int j;
+		clock_t timeElapsed = clock();
+		for(j=0;j<numOfUsers;j++){
+			User currentUser = ratings_getUser(train);
+			unsigned int* recommendedItems = lsh_getRecommendedItems(currentUser);
+			fprintf(outputFile,"%u",user_getUserID(currentUser));
+			unsigned int k;
+			for(k=0;k<5;k++) fprintf(outputFile,"\t%u",recommendedItems[k]);
+			fprintf(outputFile,"\n");
+		}
+		timeElapsed -= clock();
+
+		hammingLSH_error += validateResults(train,test);
+		lsh_terminate();
+		fprintf(outputFile,"Execution Time: %f\n",1000*(((float)t)/CLOCKS_PER_SEC));
+
+		lsh_init(train,'e');
+
+		fprintf(outputFile,"Euclidean LSH fold number %d\n",i);
+
+		unsigned int numOfUsers = ratings_getNumberOfUsers(train);
+		unsigned int j;
+		clock_t timeElapsed = clock();
+		for(j=0;j<numOfUsers;j++){
+			User currentUser = ratings_getUser(train);
+			unsigned int* recommendedItems = lsh_getRecommendedItems(currentUser);
+			fprintf(outputFile,"%u",user_getUserID(currentUser));
+			unsigned int k;
+			for(k=0;k<5;k++) fprintf(outputFile,"\t%u",recommendedItems[k]);
+			fprintf(outputFile,"\n");
+		}
+		timeElapsed -= clock();
+
+		euclideanLSH_error += validateResults(train,test);
+		lsh_terminate();
+		fprintf(outputFile,"Execution Time: %f\n",1000*(((float)t)/CLOCKS_PER_SEC));
+
+		lsh_init(train,'c');
+
+		fprintf(outputFile,"Cosine LSH fold number %d\n",i);
+
+		unsigned int numOfUsers = ratings_getNumberOfUsers(train);
+		unsigned int j;
+		clock_t timeElapsed = clock();
+		for(j=0;j<numOfUsers;j++){
+			User currentUser = ratings_getUser(train);
+			unsigned int* recommendedItems = lsh_getRecommendedItems(currentUser);
+			fprintf(outputFile,"%u",user_getUserID(currentUser));
+			unsigned int k;
+			for(k=0;k<5;k++) fprintf(outputFile,"\t%u",recommendedItems[k]);
+			fprintf(outputFile,"\n");
+		}
+		timeElapsed -= clock();
+
+		cosineLSH_error += validateResults(train,test);
+		lsh_terminate();
+		fprintf(outputFile,"Execution Time: %f\n",1000*(((float)t)/CLOCKS_PER_SEC));
+	}
+
+	if(validate){
+		outputFile = fopen(outputFilePath,"a");
+		if(outputFile == NULL){
+			perror("Failed to open output file to write validation results");
+			return -1;
+		}
+
+		fprintf(outputFile,"Hamming LSH MAE: %lf\n",hammingLSH_error/NUM_OF_FOLDS);
+		fprintf(outputFile,"Euclidean LSH MAE: %lf\n",euclideanLSH_error/NUM_OF_FOLDS);
+		fprintf(outputFile,"Cosine LSH MAE: %lf\n",cosineLSH_error/NUM_OF_FOLDS);
+
+		fclose(outputFile);
 	}
 
 	return 0;
